944c74dfd1b073e0bceac70647a287a6887efbf5
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2023-05-07 15:19:22 +0530

    client-base-commit

---
+++ pool-client/prettier.config.js
@@ -0,0 +1,11 @@
+// prettier.config.js
+module.exports = {
+    plugins: [require("prettier-plugin-tailwindcss")],
+    printWidth: 100,
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
+    bracketSpacing: true,
+    bracketSameLine: true,
+    arrowParens: "always",
+};
